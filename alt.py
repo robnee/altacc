a46c6594d7ca74c88c57a3d75d0c40d2466a01ca
Rob Nee <robnee@users.noreply.github.com>
2019-02-25 18:58:36 -0500

    add offset calc

--- alt.py
+++ alt.py
@@ -7,6 +7,23 @@ cali_data = {
 }
 
 
+def calc_offset(actpre, actcount):
+    """ compute offset from a pressure reading and known barometric pressure """
+
+    sensitivity = 54 * 5/5.1  # 54mv/kPa * 5V/5.1V = 52.94 mV/kPa
+
+    m = 5000 / 255 / sensitivity  # 5000mv / 255 counts = 0.37037 kpA/count
+
+    # actpre kPa = 0.37037 kpA/count * actcount + b kpA
+    # b kpA = actpre kpA - 0.37037 kpA/count * actcount
+
+    b = actpre - m * actcount
+    
+    offset = b / m
+
+    return offset
+
+
 def palt(press, press_0):
     """The Motorolla data sheet sez 4.5 V / 14.5 PSI which implies 4.56 V
     at 14.7 PSI.  The unit output is 209 / 14.7 while the ideal is
@@ -70,9 +87,10 @@ def palt3(press, press_0):
     alt0 = (288.14 - 288.08 * (p0 / 101.29) ** (1/5.256)) / 0.00649
     alt1 = (288.14 - 288.08 * (p1 / 101.29) ** (1/5.256)) / 0.00649
 
-    return (alt1 - alt0) * 3.2808
+    return (alt1 - alt0) * 3.2808  # converted to feet
     
 
 print(palt(189, 209))
 print(palt2(189, 209))
 print(palt3(189, 209))
+print(calc_offset(94, 209))
